d83c8f5b425f34fea79441011dfaaf2ba114d447
advance-xd <eeesandstrom@gmail.com>
2024-09-07 23:13:06 +0200

    tets

--- youtube.js
+++ youtube.js
@@ -1,6 +1,3 @@
-const main = require('./main');
-
-
 const args = process.argv.slice(2)[0].split(",");
 
 const puppeteer = require('puppeteer-extra');
@@ -24,40 +21,70 @@ const commands = {
 };
 
 async function login(account) {
-    console.log("youtube login: " + account.email)
-    browser = await main.getBrowser();
-    console.log("broswer", browser)
-
+    console.log("youtube login: " + account["email"])
+    browser = await puppeteer.launch({ headless: false, channel: "chrome"/*, userDataDir: "userDataDir/"*/ });
     page = await browser.newPage();
-    
+    console.log("broswer", browser)
     page.setDefaultTimeout(5000);
 
-    await page.goto("https://studio.youtube.com/")
+    const cookiesFileExists = fs.existsSync("./" +account.email + "/youtube.json") && fs.statSync("./" +account.email + "/youtube.json").size > 0;
+    
+    if (cookiesFileExists) {
+        
+        const cookiesString = fs.readFileSync("./" +account.email + "/youtube.json", 'utf8');
+        const cookies = JSON.parse(cookiesString);
 
-    try {
-        await page.waitForSelector('input[id="identifierId"]')
-        await page.type('input[id="identifierId"]', process.env.EMAIL)
+        // Set cookies
+        await page.setCookie(...cookies);
+    } else {
 
-        await page.click('button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 BqKGqe Jskylb TrZEUc lw1w4b"]')
+    
+    
 
-        await new Promise(resolve => setTimeout(resolve, 4000));
+        await page.goto("https://studio.youtube.com/")
 
-        await page.waitForSelector('input[type="password"]')
-        await page.type('input[type="password"]', process.env.PASSWORD)
+        try {
+            await page.waitForSelector('input[id="identifierId"]')
+            await page.type('input[id="identifierId"]', account["email"])
 
-        await page.click('button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 BqKGqe Jskylb TrZEUc lw1w4b"]')
+            await page.click('button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 BqKGqe Jskylb TrZEUc lw1w4b"]')
 
-    } catch {
-        console.log("log in")
+            await new Promise(resolve => setTimeout(resolve, 4000));
+
+            await page.waitForSelector('input[type="password"]')
+            await page.type('input[type="password"]', account["password"])
+
+            await page.click('button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 BqKGqe Jskylb TrZEUc lw1w4b"]')
+
+        } catch {
+            console.log("log in")
+        }
     }
+    await page.goto("https://www.youtube.com/upload")
+
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+    if (!fs.existsSync(account.email)) {
+        fs.mkdirSync(account.email);
+    }
+    const cookies = await page.cookies();
+
+    // Save cookies to a file
+    //fs.writeFileSync("./" +account.email + "/youtube.json", JSON.stringify(cookies, null, 4));
+
     
 }
 
 async function upload(id) {
+
+    const date = await getDayAfterXDays(await getFormattedDate(), id)
+
     const fileInputSelector = 'input[type="file"]';
     await page.waitForSelector(fileInputSelector);
     const input = await page.$(fileInputSelector);
-    const filePath = process.env.EMAIL + '/video_'+id+'.mp4'; // Update this to the path of your video file
+    const filePath = 'reddit/video_'+id+'.mp4'; // Update this to the path of your video file
     await input.uploadFile(filePath);
 
     //await page.waitForSelector('div[id="textbox"]')
@@ -69,7 +96,7 @@ async function upload(id) {
         await page.keyboard.press('Backspace');
     }
 
-    await page.type('div[id="textbox"]', topVideoPosts[id].data.title + " u/" + topVideoPosts[id].data.author + " #shorts")
+    await page.type('div[id="textbox"]', redditvideos[id].data.title + " u/" + redditvideos[id].data.author + " #shorts")
 
     await page.click('tp-yt-paper-radio-button[name="VIDEO_MADE_FOR_KIDS_NOT_MFK"]')
 
@@ -93,19 +120,20 @@ async function upload(id) {
 
     await new Promise(resolve => setTimeout(resolve, 1000));
 
-    for (let i = 0; i < 13; i++) {
+    for (let i = 0; i < 20; i++) {
         await page.keyboard.press('Backspace');
     }
 
-    const date = await getDayAfterXDays(await getFormattedDate(), id + 2)
-
+    console.log(date)
     for (let i = 0; i < date.length; i++){
         await page.keyboard.press(date[i])
+        console.log(date[i])
     }
-
+    console.log("ENTER")
     await page.keyboard.press("Enter")
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
     
     await page.click('button[aria-label="Schemalägg"]')
 
@@ -119,17 +147,61 @@ async function upload(id) {
     
 }
 
+async function getFormattedDate() {
+    return "2024-09-09"
+    const today = new Date();
+
+    
+    // Get the year, month, and day from the date
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(today.getDate()).padStart(2, '0');
+    
+    // Format the date as YYYY-MM-DD
+    return `${year}-${month}-${day}`;
+}
+
+async function getDayAfterXDays(currentDateString, x) {
+    // Parse the current date from the input string (assumes YYYY-MM-DD format)
+    const [year, month, day] = currentDateString.split('-').map(Number);
+    const currentDate = new Date(year, month - 1, day); // Months are zero-based in JavaScript Date
+  
+    // Increment the date by x days
+    const futureDate = new Date(currentDate);
+    futureDate.setDate(currentDate.getDate() + x);
+  
+    // Extract the day number from the future date
+    //const futureDay = futureDate.getDate();
+
+    const newyear = futureDate.getFullYear();
+    const newmonth = String(futureDate.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const newday = String(futureDate.getDate()).padStart(2, '0');
+  
+    return `${newyear}-${newmonth}-${newday}`;
+}
+
 let accounts = []
+let redditvideos = []
+
+async function loadreddit() {
+    const redditss = fs.readFileSync("./reddit/videosdata.json", 'utf8');
+    redditvideos = await JSON.parse(redditss);
+    console.log("redditvideos loading done")
+}
 
 async function loadaccounts() {
     const accountss = fs.readFileSync("accounts.json", 'utf8');
-    accounts = await JSON.parse(accountss)["youtube"];
+    accounts = await JSON.parse(accountss);
     console.log("youtube loading done")
-    console.log(accounts)
+    /*
     if (args[1] != "all"){
         commands[args[0]](accounts[args[1]]);
     }
-    
+    */
+    console.log(accounts["PerfectlyCutScreams"]["youtube"])
+    await loadreddit();
+    await login(accounts["PerfectlyCutScreams"]["youtube"])
+    await upload(0)
 }
 
 loadaccounts()
